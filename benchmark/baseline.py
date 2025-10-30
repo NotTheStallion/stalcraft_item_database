e1b5fb157ebdb6e05bd45d46c5c0d2b82b6b393e
The_Stallion <111058733+NotTheStallion@users.noreply.github.com>
2025-10-30 21:12:53 +0100

    [M] "benchmark/baseline.py" : Adding baseline to compare with new AI method

--- benchmark/baseline.py
+++ benchmark/baseline.py
@@ -1,9 +1,6 @@
-import numpy as np
-from itertools import product
-from sklearn.model_selection import train_test_split
-import string
 import sys
 from pathlib import Path
+from tqdm import tqdm
 
 # Ensure src is importable
 ROOT = Path(__file__).resolve().parents[1]
@@ -11,29 +8,15 @@ SRC = ROOT / "src"
 if str(SRC) not in sys.path:
     sys.path.insert(0, str(SRC))
 
-from utils.api import is_id_valid
-from dataset import encode_id, CHARS, CHAR2IDX
-from tqdm import tqdm
-import random
+from utils.api import is_id_valid  # noqa: E402
+from dataset import  generate_random_ids  # noqa: E402
 
 
-
-
-def generate_random_ids(k, length=5, seed=42):
-    max_combos = len(CHARS) ** length
-    if k > max_combos:
-        raise ValueError(f"k ({k}) is larger than the number of possible combos ({max_combos})")
-    if seed is not None:
-        random.seed(seed)
-    ids = set()
-    while len(ids) < k:
-        ids.add(''.join(random.choices(CHARS, k=length)))
-    return list(ids)
 
 
 
 def main():
-    k = 200  # set how many random ids you want
+    k = 1000  # set how many random ids you want
     
     all_ids = generate_random_ids(k)
 
@@ -43,7 +26,7 @@ def main():
             valid_ids.append(id_str)
         if (idx + 1) % 100 == 0:
             print(f"Checked {idx + 1} IDs, found {len(valid_ids)} valid so far")
-    print(f"Number of valid IDs in first 10000: {len(valid_ids)}")
+    print(f"Number of valid IDs : {len(valid_ids)}")
 
 
 
